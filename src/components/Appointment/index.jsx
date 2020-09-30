e38afd446cd3ef82c8095a06178a123f3b850287
Sepideh-tech <67478554+Sepideh-tech@users.noreply.github.com>
2020-09-30 15:40:04 -0400

    displayed appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-import classnames from 'classnames';
 
 import "./styles.scss";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
   );
 }
