04bb606325b212e86d2eb359e132a4d4fff799f3
Sepideh-tech <67478554+Sepideh-tech@users.noreply.github.com>
2020-10-01 17:08:10 -0400

    getInterview selector added

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,84 +4,46 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index";
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Cohen",
-//       interviewer: {
-//         id: 2,
-//         name: "Tori Malcolm",
-//         avatar: "https://i.imgur.com/Nmx0Qxo.png"
-//       }
-//     } 
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//     interview: {
-//       student: "Maria Butcher",
-//       interviewer: {
-//         id: 3,
-//         name: "Mildred Nazir",
-//         avatar: "https://i.imgur.com/T2WwVfS.png"
-//       }
-//     } 
-//   }
-// ];
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  //console.log(state.interviewers);
+  
 
   const setDay = day => setState({ ...state, day });
   
   useEffect(() => {
     
-    //axios.get("api/days")
-      //.then(response => setDays([...response.data]))
     Promise.all([
       axios.get('api/days'),
-      axios.get('api/appointments')
-    ]).then((all) => {
-      const [GET_DAYS, GET_APPOINTMENTS] = all;
-      // set your states here with the correct values...
-      setState(prev => ({...prev, days: GET_DAYS.data,
-        appointments: GET_APPOINTMENTS.data }));
-    })
-  },[])
+      axios.get('api/appointments'),
+      axios.get('api/interviewers')])
+        .then((all) => {
+          const [GET_DAYS, GET_APPOINTMENTS,GET_INTERVIEWERS] = all;
+          // set your states here with the correct values...
+          setState(prev => ({...prev, days: GET_DAYS.data,
+            appointments: GET_APPOINTMENTS.data,
+            interviewers: GET_INTERVIEWERS.data }));
+        })
+  },[]);
 
-  const appointmentsList = dailyAppointments.map(appointment => 
-    <Appointment key={appointment.id} {...appointment} />);
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  
+  const Schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    
+    return(<Appointment key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview} />)
+  });
   
   return (
     <main className="layout">
@@ -106,7 +68,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentsList}
+        {Schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,9 @@
+
+
 export function getAppointmentsForDay(state, day) {
   //... returns an array of appointments for that day
   
-  const selectedDay = state.days.filter(el => el.name === day)[0];
+  const selectedDay = state.days.find(el => el.name === day);
   if (!selectedDay) {
     return [];
   }
@@ -9,3 +11,14 @@ export function getAppointmentsForDay(state, day) {
   const appointmentsList = AppointmentsID.map(app => state.appointments[app.toString()]);
   return appointmentsList;
 };
+
+export function getInterview(state, interview) {
+
+  if (interview) {
+    const interviewer = {...state.interviewers[interview.interviewer.toString()]};
+    //console.log({student: interview.student , interviewer});
+    //console.log(interview);
+    return {student: interview.student , interviewer};
+  }
+  return null;
+}
