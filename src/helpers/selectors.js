710d7714e05b0a9fed0a9272bbba2f67463d74ac
Sepideh-tech <67478554+Sepideh-tech@users.noreply.github.com>
2020-09-30 21:16:27 -0400

    helper func getAppointmentsForDay added

---
+++ src/helpers/selectors.js
@@ -0,0 +1,11 @@
+export function getAppointmentsForDay(state, day) {
+  //... returns an array of appointments for that day
+  
+  const selectedDay = state.days.filter(el => el.name === day)[0];
+  if (!selectedDay) {
+    return [];
+  }
+  const AppointmentsID = selectedDay.appointments;
+  const appointmentsList = AppointmentsID.map(app => state.appointments[app.toString()]);
+  return appointmentsList;
+};
