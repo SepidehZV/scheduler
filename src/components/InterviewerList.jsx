6d805b3a4921bb2265c96f9509a3bd5493bf7ff2
Sepideh-tech <67478554+Sepideh-tech@users.noreply.github.com>
2020-10-05 16:38:18 -0400

    propType test for interviewers

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
 
-export default function InterviewerList(props) {
-  const interviewerArray = props.interviewers.map(
+function InterviewerList(props) {
+
+  const interviewersArray = props.interviewers.map(
     interviewer => <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar}
       setInterviewer={ event => props.onChange(interviewer.id)} 
       selected={props.value === interviewer.id}/>
@@ -14,7 +16,13 @@ export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewerArray} </ul>
+      <ul className="interviewers__list">{interviewersArray} </ul>
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
